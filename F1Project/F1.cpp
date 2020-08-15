fa9686b5e1ff88f2796b43474bc3c02c36b71db3
Abdulhakim-Maha <63011075@kmitl.ac.th>
2020-08-16 00:08:48 +0700

    finished

--- F1Project/F1.cpp
+++ F1Project/F1.cpp
@@ -1,7 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	printf("austiniqer");
-	printf("seoyeji")
-	return 0;
+    int age;
+    printf("Enter your age : ");
+    scanf_s("%d", &age);
+    
+    if(age >= 0 && age < 11)
+        {
+        printf("You are kid\n");
+        }
+    if (age >= 11 && age < 20)
+        {
+        printf("You are teen\n");
+        }
+    if (age >= 20 && age <40)
+        {
+        printf("You are adult\n");
+        }
+    return 0;
 }
